1a33b87354483a111fdbf278e962a137e09d6344
vvu <victor.vinh.vu@gmail.com>
2011-08-12 10:50:35 -0400

    clean up

--- builders/XMLRequestBuilder.py
+++
@@ -1,6 +0,0 @@
-import xml.etree.ElementTree as ET
-
-class XMLRequestBuilder(object):
-    
-    def __init__(self, xml):
-        self.xml=xml
--- pywebex/builders/eventBuilder.py
+++ pywebex/builders/eventBuilder.py
@@ -1,8 +1,17 @@
 import webexBuilder as build
 
 class CreateEvent(build.WebExRequestBuilder):
-    def __init__(self):
-        pass
+    def __init__(self, head, rdict):
+        self.header=head
+        self.rdict=rdict
+        self.elementlist=[]
+    def getBodyXml(self):
+        body=super(CreateEvent,self).getBodyXml()
+        bodycontent=build.ET.SubElement(body,"bodyContent")
+        bodycontent.set(build.XSI_URI+"type", "event:createEvent")
+        for e in elementlist:
+            bodycontent.append(e)
+        return body       
 
 class GetEvent(build.WebExRequestBuilder):
     def __init__(self, head, rdict):
