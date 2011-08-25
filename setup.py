4591c7fc6df231856d92d1e79b515da98099a7e5
prior <prior@cracklabs.com>
2011-08-25 16:12:51 -0400

    trying to make this installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='pywebex',
+    version='1.0',
+    description='Python WebEx Api Wrapper',
+    author='Michael Prior',
+    author_email='prior@cracklabs.com',
+    packages=['webex'],
+)
