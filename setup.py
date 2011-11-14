2ed1f5ff29ef41bece00f8a8d44e1bd4ee3a9ff0
prior <prior@cracklabs.com>
2011-11-14 16:36:23 -0500

    bump version

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.core import setup
 
 setup(
     name='pywebex',
-    version='1.2',
+    version='1.2.1',
     description='Python WebEx Api Wrapper',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/pywebex',
-    download_url='https://github.com/prior/pywebex/tarball/v1.2',
+    download_url='https://github.com/prior/pywebex/tarball/v1.2.1',
     packages=['webex'],
     install_requires=[
         'lxml==2.3.1',
         'nose==1.1.2',
         'unittest2==0.5.1',
-        'sanetime==3.0.3'],
-    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.0.3'],
+        'sanetime==3.0.4'],
+    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.0.4'],
 )
