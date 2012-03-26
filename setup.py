8ee48880d55eee8ec6bd5cff162917004995b133
prior <prior@cracklabs.com>
2012-03-25 23:28:57 -0400

    bump vesion

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ from distutils.core import setup
 
 setup(
     name='webexpy',
-    version='2.0.9',
+    version='2.1',
     description='Python WebEx Api Wrapper',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/webexpy',
-    download_url='https://github.com/prior/webexpy/tarball/v2.0.9',
+    download_url='https://github.com/prior/webexpy/tarball/v2.1',
     packages=['webex'],
     install_requires=[
         'lxml==2.3.1',
         'nose==1.1.2',
-        'unittest2==0.5.1',
-        'sanetime==3.1.2'],
-    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.1.2'],
+        'sanetime==3.2'],
+    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.2'],
 )
